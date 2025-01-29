021da24f705974dbeaf50ec20ddee4806726f317
Tmenov Dmitry <58930447+dmtconst@users.noreply.github.com>
2025-01-29 14:19:52 +0300

    222222.java

--- src/main/java/org/example/demo111/gitcheck/MainGit.java
+++ src/main/java/org/example/demo111/gitcheck/MainGit.java
@@ -3,5 +3,8 @@ package org.example.demo111.gitcheck;
 public class MainGit {
     public static void main(String[] args) {
         System.out.println("Первый коммит 2222222");
+        System.out.println("Первый коммит 2222222");
+        System.out.println("Первый коммит 2222222");
+        System.out.println("Первый коммит 2222222");
     }
 }
