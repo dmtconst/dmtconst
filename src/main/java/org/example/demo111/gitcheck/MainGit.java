19f532530c715a419c77bbfdfd1bd375ecb8270d
dmtconst <dmtconstt@yandex.ru>
2025-01-29 14:16:57 +0300

    666

--- src/main/java/org/example/demo111/gitcheck/MainGit.java
+++ src/main/java/org/example/demo111/gitcheck/MainGit.java
@@ -3,5 +3,6 @@ package org.example.demo111.gitcheck;
 public class MainGit {
     public static void main(String[] args) {
         System.out.println("Первый коммит 666");
+        System.out.println("Первый коммит 666");
     }
 }
