c9c5f64d1b398280247d068cae7a87ec6d9ec819
dmtconst <dmtconstt@yandex.ru>
2025-01-29 13:47:55 +0300

    удалил Добавил поверх без конфликта

--- src/main/java/org/example/demo111/gitcheck/MainGit.java
+++ src/main/java/org/example/demo111/gitcheck/MainGit.java
@@ -4,5 +4,7 @@ public class MainGit {
     public static void main(String[] args) {
         System.out.println("Первый коммит");
         System.out.println("создал вестку");
+
+        System.out.println("Добавил поверх без конфликта");
     }
 }
